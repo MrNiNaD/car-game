f93c1856c454d15247adc0664b97dc8fd84b6c51
MrNiNaD <ninadparkar273@gmail.com>
2024-06-16 15:10:43 +0530

    feat: initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const createSetup = () => {
+  const roadTrack = document.querySelector('.road-track');
+
+  if (roadTrack) {
+    const twoPercent = roadTrack.clientHeight * (2/100);
+    
+    const divider = `<span class="divider"></span>`;
+  }
+  
+}
+
+createSetup()
